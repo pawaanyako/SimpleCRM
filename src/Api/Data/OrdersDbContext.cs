38b2151dcfa59a1e91988d84e5cf523e953c5c27
pawaanyako <pawaanyako@gmail.com>
2026-08-31 20:03:19 +0300

    feat: add Api project with EF Core and PostgreSQL

---
+++ src/Api/Data/OrdersDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Data
+{
+    public class OrdersDbContext(DbContextOptions<OrdersDbContext> options) : DbContext(options)
+    {
+        public DbSet<Order> Orders => Set<Order>();
+    }
+}
---
+++ src/Api/Migrations/20260831162230_InitialCreate.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    SenderCity = table.Column<string>(type: "text", nullable: false),
+                    SenderAddress = table.Column<string>(type: "text", nullable: false),
+                    ReceiverCity = table.Column<string>(type: "text", nullable: false),
+                    ReceiverAddress = table.Column<string>(type: "text", nullable: false),
+                    CargoWeight = table.Column<decimal>(type: "numeric", nullable: false),
+                    CollectionDate = table.Column<DateOnly>(type: "date", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Orders");
+        }
+    }
+}
